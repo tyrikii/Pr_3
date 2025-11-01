b9393d521b685aab2be3f4800cb1b234c2ae1aac
Артур Давлетшин <artyrrobert@yandex.ru>
2025-11-02 00:48:24 +0500

    Практика 7

---
+++ models/users.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from models.events import Event
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]] = None
+    
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": []
+            }
+        }
+
+class NewUser(BaseModel):
+    email: EmailStr
+    password: str
+    username: str
+    
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "FastPackt",
+                "password": "strong!!!"
+            }
+        }
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+    
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!"
+            }
+        }
