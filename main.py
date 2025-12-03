310619419b4aa676b99160675a56cb4ce033fe29
Артур Давлетшин <artyrrobert@yandex.ru>
2025-12-04 02:28:48 +0500

    Практика 8 SQL

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from database.connection import conn # Импортируем функцию создания БД
+
 from routes.users import user_router
 from routes.events import event_router
 import uvicorn
@@ -8,6 +11,14 @@ app = FastAPI()
 # Регистрация маршрутов
 app.include_router(user_router, prefix="/user")
 app.include_router(event_router, prefix="/event")
+
+@app.on_event("startup")
+def on_startup():
+    conn() # Создаем таблицы при запуске
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event/")
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
--- models/events.py
+++ models/events.py
@@ -1,21 +1,43 @@
-from pydantic import BaseModel
-from typing import List
+from sqlmodel import JSON, SQLModel, Field, Column
+from typing import Optional, List
 
-class Event(BaseModel):
-    id: int
+# Основная модель таблицы
+class Event(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
     title: str
     image: str
     description: str
-    tags: List[str]
+    # SQLModel по умолчанию не умеет хранить списки, используем JSON
+    tags: List[str] = Field(sa_column=Column(JSON)) 
     location: str
-    
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_schema_extra = {
+            "example": {
+                "title": "FastAPI Book Launch",
+                "image": "https://linktomyimage.com/image.png",
+                "description": "We will be discussing the contents of the FastAPI book in this event.",
+                "tags": ["python", "fastapi", "book", "launch"],
+                "location": "Google Meet"
+            }
+        }
+
+# Модель для обновления (все поля необязательные)
+class EventUpdate(SQLModel):
+    title: Optional[str]
+    image: Optional[str]
+    description: Optional[str]
+    tags: Optional[List[str]]
+    location: Optional[str]
+
     class Config:
         json_schema_extra = {
             "example": {
                 "title": "FastAPI Book Launch",
                 "image": "https://linktomyimage.com/image.png",
-                "description": "Text message",
-                "tags": ["python", "fastapi", "book", "launch"],
-                "location": "Google Meet"
+                "description": "The launch of the FastAPI book will hold on xyz.",
+                "tags": ["python", "fastapi"],
+                "location": "virtual"
             }
         }
